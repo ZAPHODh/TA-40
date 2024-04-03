50e050e2edaae53121d25dc85f2b3ab241dcca92
ZAPHODh <luispaulo.ni@gmail.com>
2024-04-03 19:59:32 -0300

    additing comments

--- robotjsTALoop.ts
+++ robotjsTALoop.ts
@@ -4,40 +4,42 @@ import { wait } from './utils/await'
 
 export const TALoop = async (numberOfPots: number) => {
     try {
-        console.log('running')
+        //set up robot
         robot.setKeyboardDelay(2)
         robot.setMouseDelay(1)
 
+        //open map
         robot.keyTap('m')
 
+        //move to base
         robot.moveMouseSmooth(Mouse.home.x, Mouse.home.y)
         robot.mouseClick()
-
         robot.moveMouseSmooth(Mouse.moveHome.x, Mouse.moveHome.y)
         robot.mouseClick()
         await wait(1000)
         robot.keyTap('y')
 
+        //move to buy pot
         robot.moveMouseSmooth(Mouse.moveBuyPot.x, Mouse.moveBuyPot.y)
         robot.mouseClick()
         await wait(10000)
         robot.mouseClick()
 
+        // buy pots
         robot.moveMouseSmooth(Mouse.addHundred.x, Mouse.addHundred.y)
         for (let i = 0; i < numberOfPots; i++) {
             await wait(500)
             robot.mouseClick()
         }
 
-        // Confirm purchase
         await wait(1000)
         robot.keyTap('y')
-
-        // Close menu
         robot.keyTap('escape')
+
+        //move back
         robot.keyTap('m')
         await wait(500)
-        // Return to previous location
+
         robot.moveMouseSmooth(
             Mouse.moveBackLocation.x,
             Mouse.moveBackLocation.y
